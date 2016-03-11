f9d43b93d7880a6c4410dac42f3d5ef55ecd912d
Matteo M. <matteo.maponi@gmail.com>
2016-03-11 11:21:29 +0100

    Salvato un cookie conpCookie con angular $cookie. Creato un service authCtrl che si occuperà di verificare l'autenticazione, salvare i cookie nella local storage, interagire col login eccetera. Messa la durata dei cookie a cinque minuti. Ora dopo il login l'utente viene reindirizzato alla pagina del suo account.

---
+++ Conpartir-war/web/js/authCtrl.js
@@ -0,0 +1,59 @@
+(function () {
+  'use strict';
+
+var modAuthenticator = angular.module('authModule', ['ngRoute','ngCookies']);
+
+    //Factory per dati condivisi
+   modAuthenticator.factory('auth', [ '$http', '$cookies',
+   function ($http,$cookies) {
+        var data;
+        var obj = {};
+        
+        //variabili per le richieste SOAP
+        var SOAPbase = "http://localhost:8080/Conpartir-war/SOAPServiceClient";
+        var SOAPhead = '<?xml version="1.0" encoding="utf-8"?>' +   
+                '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
+                '<soap:Body>';
+        var SOAPtail =  '</soap:Body>' +
+                '</soap:Envelope>';
+        var x2js = new X2JS();
+         
+        return {
+            
+            isAuth: function() {
+                var res=false;
+                var cookie = sessionStorage.getItem('conpCookie');
+                if(cookie !== null) res=true;
+                return res; 
+            },
+            
+            saveCookie: function(ckName,ckValue) {
+                sessionStorage.setItem(ckName,ckValue);
+            },
+            
+            
+            getData: function () {
+                // console.log(obj + ' was returned as data');
+                 console.log("sto passando un oggetto obj = " );
+                 console.log(obj);
+                return obj;
+            },
+            
+            
+            setData: function (name,surname,email,carModel) {
+                // console.log('setting ' + data + ' as data');
+                obj.name = name;
+                obj.surname = surname;
+                obj.email = email;
+                obj.carModel = carModel;
+            }
+
+
+
+        };
+    }]);
+       
+    
+    
+
+})();
