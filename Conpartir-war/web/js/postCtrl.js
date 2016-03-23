8eb8871f15fad2c459e4bf5b898a25497ca51ece
Matteo M. <matteo.maponi@gmail.com>
2016-03-23 19:25:19 +0100

    Prova di pagina Post

--- Conpartir-war/web/js/postCtrl.js
+++ Conpartir-war/web/js/postCtrl.js
@@ -5,14 +5,62 @@
   modPost.controller('PostController', ['$scope', '$route', '$routeParams','$location','$timeout' ,'login', 'shared',
       function($scope,$route, $routeParams, $location, $timeout,login, shared) {
           
-          $scope.notLogged;
+          $scope.notLogged = true;
+          $scope.isCarTravel = false;
+          $scope.isTaxiTravel = false;
+          $scope.travel;
           
+            $(document).ready(function() {
+                $( "#datepicker" ).datepicker({
+                    dateFormat: "yyyy-mm-dd"
+                });    
+                $('#timepicker').timepicker();                       
+            });
           
           $scope.check = function () {
-              if(login.getAuth() == false) $scope.notLogged=true;
+              var flag;
+              login.getAuth().then(
+                      function successCallback(data) {  
+                    if (data.status==200) 
+                    {
+                        flag = true;                     
+                       //console.log("flag vale" + flag);
+                      
+                    }; 
+                    if (flag == true){ 
+                          $scope.notLogged = false; 
+                       }
+                       else {
+                           $scope.notLogged = true;
+                       }           
+                });
+                      
+                      
               console.log("risultato =" +$scope.notLogged);
              
               
+          };
+          
+          $scope.mode = function () {
+              if($scope.isCarTravel==true) $scope.isTaxiTravel=false;
+              if($scope.isTaxiTravel==true) $scope.isCarTravel=false;
+          };
+          
+          $scope.post = function (input) {
+               var when = $('#datepicker').datepicker({dateFormat: "yyyy-mm-dd" }).val();
+                
+                if (when !== null && when != "") {             
+                    var month = when.slice(0,2);
+                    var day = when.slice(3,5);
+                    var year= when.slice(6,10);
+                    when = day + '-' + month + '-' + year;
+             
+                    $scope.travel.when = when;
+                }  
+                /* var hour = $('#timepicker').timepicker().val();
+                
+                $scope.travel.hour = hour; */
+                console.log($scope.travel);
           };
                 
         }]);
